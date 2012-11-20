3748a2e87450ae1686ee290bf77e347cf07b4cc4
Lukasz Tomczok <ltomczok@zuk-ts.pl>
2012-11-20 11:49:00 +0100

    dodanie projektu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ksiegarnia::Application.config.secret_token = '73af059d240bc2e5f4fbf4053b62646a3604e163b312fda57190e9cfdda7a20ba1691ac158b1fc740a8e24cce1eab182c2d74b3f8c5c79f8caa2685e51f79f33'
