1722767f31a0414622bc54338270fa6fe4c5b324
Lukasz Tomczok <ltomczok@zuk-ts.pl>
2012-11-21 15:24:23 +0100

    Dzien trzecie szkolenia

---
+++ app/models/product.rb
@@ -0,0 +1,15 @@
+#coding: UTF-8
+class Product < ActiveRecord::Base
+  	attr_accessible :description, :image_url, :price, :title
+	validates :image_url,
+		  :description,
+		  :title, :presence => true
+	validates :title, :uniqueness => true
+	validates :price, numericality: { greater_than_or_equal_to: 0.01}
+	#validates :image_url, 
+	#		format:
+	#		{
+	#			with: %r{\.(gif)\Z}i,
+	#			message: 'żółć'
+	#		}
+	end
